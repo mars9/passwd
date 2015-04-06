d6225bf08b484c01b3e6003d13c36e54ce0e6fb8
mason <marraison@gmail.com>
2015-04-06 12:15:06 +0200

    1428315306

--- passwd.go
+++ passwd.go
@@ -2,15 +2,17 @@ package passwd
 
 import "errors"
 
+var ErrUnsupportedOS = errors.New("not supported")
+
 var passFunc func(prompt string) ([]byte, error)
 
-// GetPasswd displays a prompt to, and reads in a password from /dev/tty
-// (/dev/cons). GetPasswd turns off character echoing while reading the
-// password. The calling process should zero the password as soon as
-// possible.
-func GetPasswd(prompt string) ([]byte, error) {
+// Get displays a prompt to, and reads in a password from /dev/tty
+// (/dev/cons). Get turns off character echoing while reading the
+// password. The calling process should zero the password as soon
+// as possible.
+func Get(prompt string) ([]byte, error) {
 	if passFunc == nil {
-		return nil, errors.New("not supported")
+		return nil, ErrUnsupportedOS
 	}
 	return passFunc(prompt)
 }
